e796cd1bb0c42705956fabe330544085f1ac4e7d
anshu <anshu.ac.dv@gmail.com>
2026-01-17 19:52:45 +0530

    Initial Commit

--- Dynamic DataType.dart
+++ Dynamic DataType.dart
@@ -1,5 +1,14 @@
 void main() {
+  // Using dynamic data type
   var data;
+  var number = 10;
+  var text = "Hello, Dart!";
+  var isTrue = true;
+  // Using static data type
   data = {'key': 1, 'value': 2};
+  // Accessing dynamic data type
   print(data['key']);
+  print(number);
+  print(text);
+  print(isTrue);
 }
