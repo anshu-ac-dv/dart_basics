1b756ef4d0630469cdbdeb0be5c4a297f55ad63e
anshu <anshu.ac.dv@gmail.com>
2026-01-17 20:29:12 +0530

    Initial Commit

---
+++ Input.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter one number: ');
+  int? number1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Enter another number: ');
+  int? number2 = int.parse(stdin.readLineSync()!);
+
+  var sum = number1 + number2;
+  print('The sum of $number1 and $number2 is $sum.');
+}
--- UserInput.dart
+++ UserInput.dart
@@ -3,5 +3,8 @@ import 'dart:io';
 void main() {
   print('Enter your name:');
   String? name = stdin.readLineSync();
-  print('Hello, $name!');
+  print('Your name is $name!');
+  print('Enter your age:');
+  int? age = int.parse(stdin.readLineSync()!);
+  print('You are $age years old!');
 }
