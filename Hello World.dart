bcb3743f28144dc2e7d273e0bf72fa448e5b44a0
anshu <anshu.ac.dv@gmail.com>
2026-01-17 16:35:41 +0530

    Initial Commit

--- Hello World.dart
+++ Hello World.dart
@@ -1,3 +1,18 @@
 void main() {
+  // This is a simple Dart program that prints "Hello, World!" to the console.
   print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
+  // Repeated calls to demonstrate multiple outputs
+  print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
+  /* 
+  print('Hello, World!');
+  print('Hello, World!'); 
+  print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
+  */
 }
