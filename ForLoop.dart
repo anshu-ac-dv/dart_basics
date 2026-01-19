e756e4727ab95932f36414b7df49508f4fc2f39b
anshu <anshu.ac.dv@gmail.com>
2026-01-19 16:42:03 +0530

    Initial Commit

--- ForLoop.dart
+++ ForLoop.dart
@@ -1,6 +1,9 @@
 void main() {
+  // Declare an integer variable x and assign it the value 10
   int x = 10;
+  // Simple for loop that prints numbers from 0 to x-1
   for (var i = 0; i < x; i++) {
+    // Print the current value of i
     print(i);
   }
 }
