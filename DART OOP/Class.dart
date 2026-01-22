bf8277c2d3467b504cecf5c9edace7b7c1409df2
anshu <anshu.ac.dv@gmail.com>
2026-01-22 20:50:12 +0530

    Initial Commit

--- DART OOP/Class.dart
+++ DART OOP/Class.dart
@@ -1,15 +1,20 @@
 void main() {
+  // Creating an instance of the Person class
   Person person = Person();
   person.name = 'Anshu Kumar';
   person.age = 21;
   person.isEmployed = true;
+  // Calling the displayInfo method to display person information
   person.displayInfo();
 }
 
+// Person class
 class Person {
+  // Instance variables
   String? name;
   int? age;
   bool? isEmployed;
+  // Method to display person information
   void displayInfo() {
     print('Name: $name');
     print('Age: $age');
