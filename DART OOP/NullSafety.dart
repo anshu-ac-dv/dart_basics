c7308c4acfb3a4ab1fcfe46a81e286a1cf37926e
anshu <anshu.ac.dv@gmail.com>
2026-01-22 20:19:55 +0530

    Initial Commit

---
+++ DART OOP/NullSafety.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Nullable variable declarations
+  int? day;
+  String? dayString;
+  // Assigning null to nullable variables
+  print(day);
+  print(dayString);
+
+  // Assigning non-null values to nullable variables
+  day = 5;
+  print(day);
+}
