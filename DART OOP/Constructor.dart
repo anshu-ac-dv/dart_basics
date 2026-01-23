a3fac3a22107707af33506aad2553f85b80c7d35
anshu <anshu.ac.dv@gmail.com>
2026-01-23 20:08:50 +0530

    Initial Commit

--- DART OOP/Constructor.dart
+++ DART OOP/Constructor.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  Student student1 = Student();
+  student1.Display();
+}
+
+class Student {
+  String? name;
+  int? age;
+
+  Student() {
+    name = "John Doe";
+    age = 20;
+  }
+
+  void Display() {
+    print("Name: $name, Age: $age");
+  }
+}
