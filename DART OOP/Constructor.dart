40fcf1eb6de6d0e485bf60d422364b3307acc2d8
anshu <anshu.ac.dv@gmail.com>
2026-01-24 21:24:29 +0530

    Initial Commit

--- DART OOP/Constructor.dart
+++ DART OOP/Constructor.dart
@@ -1,4 +1,5 @@
 void main() {
+  // Creating a list of Student objects
   List<Student> students = [
     Student("Anshu", 22),
     Student("Ankita", 22),
@@ -6,6 +7,7 @@ void main() {
     Student("Anshu", 22),
   ];
 
+  // Displaying student details
   students.forEach((student) {
     print("----- Student Details -----");
     // print("Name: ${student.name}, Age: ${student.age}");
@@ -13,15 +15,19 @@ void main() {
   });
 }
 
+// Student class with name and age properties
 class Student {
+  // name and age properties
   String? name;
   int? age;
 
+  // Constructor to initialize name and age
   Student(String name, int age) {
     this.name = name;
     this.age = age;
   }
 
+  // Method to display student details
   void Display() {
     print("Name: $name, Age: $age");
   }
